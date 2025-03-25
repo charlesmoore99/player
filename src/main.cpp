cd58ab939b8027025679d200763b416f7d137510
charles.moore <cmoore@appliedres.com>
2025-03-25 09:43:27 -0400

    added rapidjson

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,44 @@
 #include <fmt/core.h>
 #include <cmath>
+#include <chrono>
+#include <thread>
 
-std::string getEnvString(std::string name, std::string defaultVal) {
-    const char* valPtr = std::getenv(name.c_str());
-    if (!valPtr) {
+std::string getEnvString(std::string name, std::string defaultVal)
+{
+    const char *valPtr = std::getenv(name.c_str());
+    if (!valPtr)
+    {
         return defaultVal;
     }
     std::string val = valPtr;
     return val;
 }
 
-double getEnvDouble(std::string name, double defaultVal) {
-    const char* valPtr = std::getenv(name.c_str());
-    if (!valPtr) {
+double getEnvDouble(std::string name, double defaultVal)
+{
+    const char *valPtr = std::getenv(name.c_str());
+    if (!valPtr)
+    {
         return defaultVal;
     }
-    double val = std::stod(valPtr);;
+    double val = std::stod(valPtr);
+    ;
     return val;
 }
 
-
-
-struct Coordinates {
+struct Coordinates
+{
     double latitude;
     double longitude;
 };
 
 std::tuple<double, double> calculateDestination(
-    const double &beginLatDeg, 
-    const double &beginLonDeg, 
-    const double &bearingDeg, 
-    const double &speedKPH, 
-    const double &timeH) {
+    const double &beginLatDeg,
+    const double &beginLonDeg,
+    const double &bearingDeg,
+    const double &speedKPH,
+    const double &timeH)
+{
 
     const double R = 6371.0; // Earth's radius in kilometers
 
@@ -56,9 +63,9 @@ std::tuple<double, double> calculateDestination(
     return {endLat, endlon};
 }
 
-class Player {
+class Player
+{
 public:
-
     std::string name;
     double lat;
     double lon;
@@ -68,23 +75,34 @@ public:
     double kph;
 
     inline Player(
-        const std::string & playerName, 
-        const double &playerLat, 
-        const double &playerLon, 
-        const double &playerAlt, 
-        const double &playerBearing, 
-        const double &playerKph) 
-    : name(playerName), lat(playerLat), lon(playerLon), alt(playerAlt), bearing(playerBearing), kph(playerKph) 
-    {}
+        const std::string &playerName,
+        const double &playerLat,
+        const double &playerLon,
+        const double &playerAlt,
+        const double &playerBearing,
+        const double &playerKph)
+        : name(playerName), lat(playerLat), lon(playerLon), alt(playerAlt), bearing(playerBearing), kph(playerKph)
+    {
+    }
 
+    inline std::string toString()
+    {
+        return fmt::format("Player Name: \"{}\", Coords:({}, {}, {}), Velocity: ({}, {})", name, lat, lon, alt, bearing, kph);
+    }
+
+    inline void updateDestination(double hours)
+    {
+        std::tie(lat, lon) = calculateDestination(lat, lon, bearing, kph, hours);
+    }
 };
 
+int main()
+{
 
-int main() {
+    try
+    {
 
-    try {
-
-        // read vars from env 
+        // read vars from env
         std::string playerName = getEnvString("PLAYER_NAME", "Bob");
 
         // location
@@ -96,40 +114,63 @@ int main() {
         double bearing = getEnvDouble("PLAYER_BEARING_DEG", 40.0);
         double rate = getEnvDouble("PLAYER_RATE", 50.0);
 
-
-        if (lat < -90.0 || lat > 90.0) {
+        if (lat < -90.0 || lat > 90.0)
+        {
             throw std::out_of_range(fmt::format("Latitude value ({}) is out of range. It must be in the range (-90.0 , 90.0)", lat));
         }
 
-        if (lon < -180.0 || lon > 180.0) {
+        if (lon < -180.0 || lon > 180.0)
+        {
             throw std::out_of_range(fmt::format("Longitude value ({}) is out of range. It must be in the range [-180.0 , 180.0)", lon));
         }
 
-        if (bearing < 0.0 || bearing >= 360.0) {
+        if (bearing < 0.0 || bearing >= 360.0)
+        {
             throw std::out_of_range(fmt::format("Bearing value ({}) is out of range. It must be in the range (0.0->360.0]", bearing));
         }
 
-        if (rate < 0.0) {
+        if (rate < 0.0)
+        {
             throw std::out_of_range(fmt::format("Rate value ({}) is out of range.  It must be greather than or equal to 0.", bearing));
         }
 
-        fmt::print("Player Name: {} ({}, {}, {}).  {} {}!\n", playerName, lat, lon, alt, bearing, rate);
+        Player p(playerName, lat, lon, alt, bearing, rate);
+        fmt::print("{}\n", p.toString());
 
+        // event loop to update the player location
+        auto lastUpdateTime = std::chrono::high_resolution_clock::now();
 
+        double updateRate = 1;
+        for (unsigned int x = 0; x < 100; x++)
+        { // Game loop (replace with your exit condition)
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> deltaTime = currentTime - lastUpdateTime;
+            lastUpdateTime = currentTime;
 
+            int sec = std::chrono::duration_cast<std::chrono::nanoseconds>(deltaTime).count();
+            double hours = sec / (3600.0 * 1e9);
+            fmt::print("{}\n", hours);
+            p.updateDestination(hours);
 
-    } catch (const std::out_of_range &e) {
+            fmt::print("{}\n", p.toString());
+
+            std::this_thread::sleep_for(std::chrono::duration<double>(1.0 / updateRate)); // Control update rate
+        }
+    }
+    catch (const std::out_of_range &e)
+    {
         fmt::print("Data Validation exception caught: {} !\n", e.what());
         return 1;
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
         fmt::print("Exception caught: {} !\n", e.what());
         return 1;
-    } catch (...) {
+    }
+    catch (...)
+    {
         fmt::print("Unknown Exception caught!\n");
         return 1;
     }
-
     return 0;
 }
-
-
